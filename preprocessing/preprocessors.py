359dda3983ee13d4c7755e01a35ad8e13e158d2c
arefehzrf <56091322+arefehzrf@users.noreply.github.com>
2024-01-28 08:36:38 +0100

    Update preprocessors.py
    vix added

--- preprocessing/preprocessors.py
+++ preprocessing/preprocessors.py
@@ -118,36 +118,9 @@ def add_turbulence(df):
 
 
 def calcualte_turbulence(df):
-    """calculate turbulence index based on dow 30"""
-    # can add other market assets
-    
-    df_price_pivot=df.pivot(index='datadate', columns='tic', values='adjcp')
-    unique_date = df.datadate.unique()
-    # start after a year
-    start = 252
-    turbulence_index = [0]*start
-    #turbulence_index = [0]
-    count=0
-    for i in range(start,len(unique_date)):
-        current_price = df_price_pivot[df_price_pivot.index == unique_date[i]]
-        hist_price = df_price_pivot[[n in unique_date[0:i] for n in df_price_pivot.index ]]
-        cov_temp = hist_price.cov()
-        current_temp=(current_price - np.mean(hist_price,axis=0))
-        temp = current_temp.values.dot(np.linalg.inv(cov_temp)).dot(current_temp.values.T)
-        if temp>0:
-            count+=1
-            if count>2:
-                turbulence_temp = temp[0][0]
-            else:
-                #avoid large outlier because of the calculation just begins
-                turbulence_temp=0
-        else:
-            turbulence_temp=0
-        turbulence_index.append(turbulence_temp)
-    
-    
-    turbulence_index = pd.DataFrame({'datadate':df_price_pivot.index,
-                                     'turbulence':turbulence_index})
+    df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+    vix_data = yf.download('^VIX', start=df.iloc[0][0], end=df.iloc[-1][0])['Close']
+    turbulence_index = pd.DataFrame(vix_data)
     return turbulence_index
 
 
